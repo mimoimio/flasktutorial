9963143904c223a22662eedfca41aaebd0946777
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-05-20 02:05:36 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: [
+    './flaskr/templates/**/*.html', // Jinja templates
+    './flaskr/static/**/*.js',      // any JS with class strings
+  ],
+  theme: { extend: {} },
+};
